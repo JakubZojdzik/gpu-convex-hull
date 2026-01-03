bcff036e46dd4cc31e617e30b712f36698e6cc40
kumber <jakub.zojdzik@gmail.com>
2026-01-03 14:50:36 +0100

    angle computation

--- cpu_graham.cpp
+++ cpu_graham.cpp
@@ -2,15 +2,15 @@
 #include "utils.h"
 using namespace std;
 
-// >0 - R is on the left of PQ, =0 - R is in line with PQ, <0 - R is on the right of PQ
-// Return values: 1 = left turn (counter-clockwise), 0 = collinear, -1 = right turn (clockwise)
-static int orientation(const Point &p, const Point &q, const Point &r) {
-    float val = (q.x - p.x)*(r.y - p.y) - (q.y - p.y)*(r.x - p.x);
-    if (val == 0.0f) return 0;
-    return (val > 0.0f) ? 1 : -1;
+// Cross product of vectors (p1-p0) and (p2-p0)
+// >0 means p2 is left of line p0->p1 (counter-clockwise)
+// =0 means collinear
+// <0 means p2 is right of line p0->p1 (clockwise)
+static float cross(const Point &p0, const Point &p1, const Point &p2) {
+    return (p1.x - p0.x) * (p2.y - p0.y) - (p1.y - p0.y) * (p2.x - p0.x);
 }
 
-static float dist(const Point &p, const Point &q) {
+static float dist2(const Point &p, const Point &q) {
     return (p.x - q.x)*(p.x - q.x) + (p.y - q.y)*(p.y - q.y);
 }
 
@@ -27,6 +27,7 @@ void grahamScan(float *p_x, float *p_y, int N, float *result_x, float *result_y,
     vector<Point> points(N);
     for (int i = 0; i < N; i++) points[i] = {p_x[i], p_y[i]};
 
+    // Find bottom-most point (lowest y, then leftmost x)
     int ymin = 0;
     for (int i = 1; i < N; i++)
         if (points[i].y < points[ymin].y || (points[i].y == points[ymin].y && points[i].x < points[ymin].x))
@@ -35,24 +36,24 @@ void grahamScan(float *p_x, float *p_y, int N, float *result_x, float *result_y,
     swap(points[0], points[ymin]);
     Point p0 = points[0];
 
+    // Sort by polar angle using atan2 (robust, transitive comparison)
     auto less_than = [p0](const Point &p1, const Point &p2) {
-        int o = orientation(p0, p1, p2);
-        if (o == 0) return dist(p0, p1) < dist(p0, p2);
-        // place points with smaller polar angle first (counter-clockwise ordering)
-        return o == 1;
+        float angle1 = atan2f(p1.y - p0.y, p1.x - p0.x);
+        float angle2 = atan2f(p2.y - p0.y, p2.x - p0.x);
+        if (angle1 != angle2) return angle1 < angle2;
+        // For collinear points, closer point comes first
+        return dist2(p0, p1) < dist2(p0, p2);
     };
 
     sort(points.begin()+1, points.end(), less_than);
 
+    // Build hull
     vector<Point> hull;
     hull.push_back(points[0]);
-    hull.push_back(points[1]);
-    hull.push_back(points[2]);
 
-    for (int i = 3; i < N; i++) {
-        // While the sequence of last two points and the new point does not make a left turn,
-        // pop the last point. We want to maintain counter-clockwise (left) turns.
-        while (hull.size() >= 2 && orientation(hull[hull.size()-2], hull[hull.size()-1], points[i]) != 1)
+    for (int i = 1; i < N; i++) {
+        // Pop while we make a right turn or go straight
+        while (hull.size() >= 2 && cross(hull[hull.size()-2], hull[hull.size()-1], points[i]) <= 0)
             hull.pop_back();
         hull.push_back(points[i]);
     }
