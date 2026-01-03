c7cad6b60962ce99d7552f86447eb4be19b334ca
kumber <jakub.zojdzik@gmail.com>
2026-01-03 21:14:48 +0100

    set problematic input

--- main.cu
+++ main.cu
@@ -50,22 +50,40 @@ int main()
 {
     int N = 30;
 
-    float *px = (float*) malloc(sizeof(float) * N);
-    float *py = (float*) malloc(sizeof(float) * N);
+    // float *px = (float*) malloc(sizeof(float) * N);
+    // float *py = (float*) malloc(sizeof(float) * N);
 
-    srand(time(NULL));
+    // srand(time(NULL));
 
-    int points = 0;
-    float radius = 10000.0f;
-    while (points < N) {
-        float x = radius * 2.0f * (rand() / (float)RAND_MAX) - radius;
-        float y = radius * 2.0f * (rand() / (float)RAND_MAX) - radius;
-        if (x*x + y*y <= radius*radius) {
-            px[points] = x;
-            py[points] = y;
-            points++;
-        }
-    }
+    // int points = 0;
+    // float radius = 10000.0f;
+    // while (points < N) {
+    //     float x = radius * 2.0f * (rand() / (float)RAND_MAX) - radius;
+    //     float y = radius * 2.0f * (rand() / (float)RAND_MAX) - radius;
+    //     if (x*x + y*y <= radius*radius) {
+    //         px[points] = x;
+    //         py[points] = y;
+    //         points++;
+    //     }
+    // }
+    float *px = new float[N]{
+        5861.843f, -7883.658f, -7156.774f, -7887.822f, 3757.629f,
+        2383.592f, 5502.341f, -6789.278f, 2519.980f, -2043.839f,
+        5122.345f, 2577.598f, 1792.777f, -5110.984f, -2623.934f,
+        -8583.311f, 7381.590f, 3621.583f, 660.616f, -6811.547f,
+        1278.297f, 459.991f, 6219.344f, -1606.287f, 3004.084f,
+        6903.475f, -3202.690f, 1339.803f, -5182.649f, 4080.470f
+    };
+
+    float *py = new float[N]{
+        966.890f, -5072.616f, -2101.514f, -1629.504f, 6149.915f,
+        -8782.118f, -1996.346f, 6702.553f, 4663.176f, -9240.954f,
+        3457.333f, 4789.114f, 4693.940f, -6841.067f, -4733.226f,
+        -622.064f, -4437.701f, -98.430f, 7820.965f, -4217.040f,
+        5766.052f, -9711.430f, -611.857f, -2262.690f, 7419.375f,
+        4420.772f, 9255.053f, -5821.100f, -5038.614f, -4957.175f
+    };
+
 
     printf("Input Points:\n");
     printf("[");
